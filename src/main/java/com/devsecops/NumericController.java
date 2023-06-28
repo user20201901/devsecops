296c513f93f21bbde70d8a78192ea1ae6591d845
mm <mm@example.com>
2023-06-28 12:21:16 +0200

    NumericController removed import

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -7,10 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 
 @RestController
 public class NumericController {
